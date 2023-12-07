a28fc61fd50e5db20bd4eb99d8ed6ba80997145a
Robert Buskirk-Lechner <buskirklechner@gmail.com>
2023-12-07 15:38:52 -0500

    added simple display of events to day-display

--- client-calendarapp/src/app/day-display/day-display.component.ts
+++ client-calendarapp/src/app/day-display/day-display.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import DayModel from '../../shared/models/day-model';
 import { dateStore } from '../../shared/signals/DateStore';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-day-display',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './day-display.component.html',
   styleUrl: './day-display.component.scss'
 })
@@ -28,4 +29,9 @@ export class DayDisplayComponent {
   }
 
   hours = this.generateHours()
+
+  events = [
+    {name: 'event 1', start: 9, end: 10},
+    {name: 'event 2', start: 14, end: 16},
+  ]
 }
